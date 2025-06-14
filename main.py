ae48666a5d6d797d4382caeb9fdcf99852ec05cd
Wan Danial Aiman <wan.danial.azwan@gmail.com>
2025-06-14 08:31:08 +0800

    Fix start command to use main.py

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ def stats():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # guna PORT dari Railway
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
